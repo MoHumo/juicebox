64a4fe32f5637be21151b02aba462086aa75cbbb
monique <baezamonique01@gmail.com>
2022-07-31 16:17:58 -0600

    my commit message

---
+++ db/index.js
@@ -0,0 +1,36 @@
+// inside db/index.js
+const { Client } = require('pg') // imports the pg module
+
+const client = new Client('postgres://localhost:5432/juicebox-dev');
+
+async function createUser({ 
+   username,
+   password,
+   name,
+   location
+   }) {
+  try {
+    const { rows } = await client.query(`
+      INSERT INTO users(username, password, name, location) 
+      VALUES($1, $2, $3, $4) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
+    `, [username, password, name, location]);
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getAllUsers() {
+  const { rows } = await client.query(`SELECT id, username, name, location, active FROM users;`);
+  console.log(getAllUsers);
+  return rows;
+}
+
+module.exports = {  
+  client,
+  createUser,
+  getAllUsers,
+}
